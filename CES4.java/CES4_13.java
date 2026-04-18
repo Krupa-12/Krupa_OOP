8a00a7f1dd10c8b337b50e60349abd1ca17edc94
Krupa  J. Baldha <krupabld12@gmail.com>
2026-04-19 00:35:30 +0530

    Add Shape classes to calculate area of Triangle and Rectangle
    This program calculates the area of a triangle and a rectangle based on user input.

---
+++ CES4.java/CES4_13.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Shape {
+    double d1, d2;
+    void getData(double d1, double d2) {
+        this.d1 = d1;
+        this.d2 = d2;
+    }
+}
+class Triangle extends Shape {
+    double getArea() {
+        return 0.5 * d1 * d2;
+    }
+}
+class Rectangle extends Shape {
+    double getArea() {
+        return d1 * d2;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base of Triangle: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter height of Triangle: ");
+        double h = sc.nextDouble();
+        Triangle t = new Triangle();
+        t.getData(b, h);
+        System.out.print("\nEnter length of Rectangle: ");
+        double l = sc.nextDouble();
+        System.out.print("Enter width of Rectangle: ");
+        double w = sc.nextDouble();
+        Rectangle r = new Rectangle();
+        r.getData(l, w);
+        System.out.println("\n--- Results ---");
+        System.out.println("Area of Triangle: " + t.getArea());
+        System.out.println("Area of Rectangle: " + r.getArea());
+        sc.close();
+    }
+}
